9ee3b9fc7e3fad006f3d3bbc4ade86f17874379e
Yukiko Kida <YUKIKO@M-Yukiko-Kida-MacBook-Pro.local>
2018-02-04 17:28:56 +0900

    Initial Commit

---
+++ LfSCount/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LfSCount
+//
+//  Created by Yukiko Kida on 2018/02/04.
+//  Copyright © 2018年 Yukiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
