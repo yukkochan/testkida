3be631957e3324877951bf0f33898741f526b1ed
Yukiko Kida <YUKIKO@M-Yukiko-Kida-MacBook-Pro.local>
2018-02-05 16:49:28 +0900

    first commit

--- LfSCount/ViewController.swift
+++ LfSCount/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,41 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus () {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus () {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func kakeru () {
+        number = number * 2
+        label.text = String(number)
+    }
+    
+    @IBAction func waru () {
+        number = number / 2
+        label.text = String(number)
+    }
+    
+    @IBAction func cancel () {
+        number = number - number
+        label.text = String(number)
+    }
+    
+    @IBAction func input() {
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else if number <= -10 {
+            label.textColor = UIColor.blue
+        } else {
+            label.textColor = UIColor.black
+        }
     }
 
 
